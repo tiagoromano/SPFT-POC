e0b3aaa5c173aab9e8ba5262f58141e599437207
Rodrigo Reis <digo.santos.reis@gmail.com>
2019-03-27 18:05:51 +0000

    tela de aprovação

--- src/main/mobileapp/www/js/hostApp.js
+++ src/main/mobileapp/www/js/hostApp.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-window.hostApp = 'https://11-24-40395.debug.ide.cronapp.io/';
-=======
-window.hostApp = 'https://12-50-44479.debug.ide.cronapp.io/';
->>>>>>> branch 'master' of https://github.com/tiagoromano/SPFT-POC
+window.hostApp = 'https://9-126-46633.debug.ide.cronapp.io/';
