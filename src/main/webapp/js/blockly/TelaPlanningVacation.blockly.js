7e6f96843f03e73752b8b2d9a2e8587ae5b605e8
Lincoln Roberto Martins Minto <lincoln.minto@cronapp.io>
2019-03-30 00:48:56 +0000

    Added get History buttom

--- src/main/webapp/js/blockly/TelaPlanningVacation.blockly.js
+++ src/main/webapp/js/blockly/TelaPlanningVacation.blockly.js
@@ -7,7 +7,7 @@ window.blockly.js.blockly.TelaPlanningVacation = window.blockly.js.blockly.TelaP
  * TelaPlanningVacation
  */
 window.blockly.js.blockly.TelaPlanningVacation.obterUsuariosSelecionados = function() {
- var item;
+ var item, event, x;
   item = this.cronapi.util.executeJavascriptReturn('$(\'#cron-crn-enterprise-combobox-multiple-675758\').data(\'kendoMultiSelect\').value()');
   return item;
 }
@@ -15,21 +15,34 @@ window.blockly.js.blockly.TelaPlanningVacation.obterUsuariosSelecionados = funct
 /**
  * Descreva esta função...
  */
-window.blockly.js.blockly.TelaPlanningVacation.changeUserSelectionHandler = function() {
- var item;
-}
-
-/**
- * Descreva esta função...
- */
 window.blockly.js.blockly.TelaPlanningVacation.recarregarGrafico = function() {
- var item;
+ var item, event, x;
   this.cronapi.util.executeJavascriptNoReturn('renderChart()');
 }
 
 /**
  * Descreva esta função...
  */
-window.blockly.js.blockly.TelaPlanningVacation.initCombo = function() {
- var item;
+window.blockly.js.blockly.TelaPlanningVacation.abrirModalHistorico = function(event) {
+ var item, x;
+  if (event == 'success') {
+    if (this.cronapi.util.executeJavascriptReturn('Historico.data.length') != 0) {
+      this.cronapi.screen.showModal("modalHistorico");
+    } else {
+      this.cronapi.screen.notify('info','Não existem registros disponíveis para o registro selecionado.');
+    }
+  } else if (event == 'canceled') {
+    this.cronapi.screen.notify('info','Não foi possível obter os registros. Por favor tente novamente.');
+  } else {
+    this.cronapi.util.executeJavascriptNoReturn('let that = this;\nHistorico.fetch({$inlinecount: \"allpages\", $format: \"json\", $top: \"100\"}, {\nsuccess: function() {\n    that.blockly.js.blockly.TelaPlanningVacation.abrirModalHistorico(\'succe' +
+     'ss\')\n},\ncanceled: function() {\n    that.blockly.js.blockly.TelaPlanningVacation.abrirModalHistorico(\'canceled\')\n}}, false)');
+  }
+}
+
+/**
+ * Descreva esta função...
+ */
+window.blockly.js.blockly.TelaPlanningVacation.fecharModalHistorico = function() {
+ var item, event, x;
+  this.cronapi.screen.hideModal("modalHistorico");
 }
