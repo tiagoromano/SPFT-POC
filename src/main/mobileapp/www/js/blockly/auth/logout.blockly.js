3eb9fff8fb2ed11f1c56ffa7c8d55fc5e668e3ee
Rodrigo Reis <digo.santos.reis@gmail.com>
2019-04-02 20:48:24 +0000

    mudança em login

--- src/main/mobileapp/www/js/blockly/auth/logout.blockly.js
+++ src/main/mobileapp/www/js/blockly/auth/logout.blockly.js
@@ -9,7 +9,7 @@ window.blockly.js.blockly.auth.Logout = window.blockly.js.blockly.auth.Logout ||
  */
 window.blockly.js.blockly.auth.Logout.logout = function() {
  var item;
-  this.cronapi.util.getURLFromOthers('GET', 'application/x-www-form-urlencoded', 'https://s6006as2390.transp.biz:9443/commonauth?commonAuthLogout=true&amp;type=oid&amp;commonAuthCallerPath=http://s6006as2391.transp.biz:8889/logout', null, null, function(sender_item) {
+  this.cronapi.util.getURLFromOthers('POST', 'application/x-www-form-urlencoded', 'https://s6006as2390.transp.biz:9443/commonauth?commonAuthLogout=true&amp;type=oid&amp;commonAuthCallerPath=http://s6006as2391.transp.biz:8889/logout', null, null, function(sender_item) {
       item = sender_item;
     this.cronapi.screen.logout();
   }.bind(this), function(sender_item) {
